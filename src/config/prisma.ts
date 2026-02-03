5a7462f0086ea818d08943c85860d3ee49c11128
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2026-02-03 23:40:39 +0600

    back to prisma 6

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,10 +1,3 @@
-import envVariables from './env';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const connectionString = envVariables.DATABASE_URL;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export const prisma = new PrismaClient();
